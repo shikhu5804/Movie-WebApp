41a26b6b0a8513f03bd25a47372cda7d51f88ad2
Shikhar <shikharvrm123@gmail.com>
2024-12-22 13:48:51 +0530

    React Major Project

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 const ContactShikhar = () => {
-
-  document.title="Contact Me"
+  document.title = "Contact Me";
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white p-5">
       <div className="max-w-4xl w-full bg-gray-800 text-white shadow-2xl rounded-lg overflow-hidden transform transition-transform hover:scale-105 duration-500">
@@ -19,17 +18,17 @@ const ContactShikhar = () => {
               <i className="ri-mail-line text-2xl text-blue-500 mr-3"></i>
               <span className="text-gray-300 font-medium">Email:</span>{" "}
               <a
-                href="mailto:shikhar.verma@example.com"
+                href="mailto:shikharvrm123@gmail.com"
                 className="ml-2 underline text-blue-400 hover:text-blue-500 font-semibold"
               >
-                shikhar.verma@example.com
+                shikahrvrm123@gmail.com{" "}
               </a>
             </p>
             <p className="flex items-center">
               <i className="ri-linkedin-box-line text-2xl text-blue-500 mr-3"></i>
               <span className="text-gray-300 font-medium">LinkedIn:</span>{" "}
               <a
-                href="https://www.linkedin.com/in/shikhar-verma/"
+                href="https://www.linkedin.com/in/shikhar-verma-7b6a34304/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="ml-2 underline text-blue-400 hover:text-blue-500 font-semibold"
@@ -41,12 +40,12 @@ const ContactShikhar = () => {
               <i className="ri-github-line text-2xl text-blue-500 mr-3"></i>
               <span className="text-gray-300 font-medium">GitHub:</span>{" "}
               <a
-                href="https://github.com/shikhar-verma"
+                href="https://github.com/shikhu5804"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="ml-2 underline text-blue-400 hover:text-blue-500 font-semibold"
               >
-                github.com/shikhar-verma
+                github.com/shikhu5804
               </a>
             </p>
           </div>
