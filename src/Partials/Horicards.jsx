8f49ed0d4fc1f41f7f4198a1f099f61ed14c39ca
Shikhar <shikharvrm123@gmail.com>
2025-03-24 23:59:04 +0530

    Swiper.js

--- src/Partials/Horicards.jsx
+++ src/Partials/Horicards.jsx
@@ -3,9 +3,12 @@ import axios from "../Utiliss/axios";
 import noimage from "/noimage.png";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
-import Loading from "../Partials/Loading";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Mousewheel, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 
-const Horicards = ({ setLoading }) => {
+const Horicards = () => {
   const [trending, setTrending] = useState([]);
   const [category, setCategory] = useState("all");
 
@@ -13,8 +16,6 @@ const Horicards = ({ setLoading }) => {
     try {
       const { data } = await axios.get(`/trending/${categoryType}/day`);
       setTrending(data.results);
-
-    
     } catch (error) {
       console.log("Error:", error);
     }
@@ -28,16 +29,17 @@ const Horicards = ({ setLoading }) => {
     getTrending(category);
   }, [category]);
 
-  if (!trending) {
+  if (!trending.length) {
     return (
-      <h1 className="text-white absolute right-0 bg-black font-black flex items-center justify-center text-5xl h-screen w-screen">
-        <Loading />
-      </h1>
+      <div className="text-white text-5xl font-bold flex items-center justify-center h-screen">
+        Loading...
+      </div>
     );
   }
 
-  return trending ? (
-    <div className="p-5 mb-9">
+  return (
+    <div className="p-6">
+      {/* Dropdown Filter */}
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-white text-3xl font-black">Trending</h1>
         <Dropdown onSelectChange={handleSelectChange} title="Category">
@@ -47,44 +49,64 @@ const Horicards = ({ setLoading }) => {
         </Dropdown>
       </div>
 
-      <div className="flex gap-x-6 overflow-x-auto pb-5">
+      <Swiper
+        modules={[EffectCoverflow, Mousewheel, Autoplay]}
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 150,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        autoplay={{
+          delay: 50000,
+          disableOnInteraction: false,
+        }}
+        mousewheel={{
+          forceToAxis: true,
+          sensitivity: 1,
+        }}
+        className="max-w-[1400px]  mx-auto"
+      >
         {trending.map((d, i) => (
-          <Link
-            to={`/${d.media_type}/details/${d.id}`}
+          <SwiperSlide
             key={i}
-            className="min-w-[16%] transform hover:scale-105 transition-transform duration-300 bg-zinc-900 rounded-2xl shadow-lg overflow-hidden hover:shadow-xl"
+            style={{ width: "320px", height: "450px" }}
+            className="bg-[#6D28D9] rounded-2xl shadow-lg min-h-[60vh] overflow-hidden hover:shadow-xl transition-transform duration-300"
           >
-            <img
-              className="h-[30vh] w-full object-cover"
-              src={
-                d.backdrop_path || d.poster_path
-                  ? `https://image.tmdb.org/t/p/original/${
-                      d.backdrop_path || d.poster_path
-                    }`
-                  : noimage
-              }
-              alt={d.name || d.title || "Image not available"}
-            />
-            <div className="p-4">
-              <h1 className="text-lg font-bold text-white truncate">
-                {d.name || d.title || d.original_name || d.original_title}
-              </h1>
-              <p className="text-sm text-zinc-400 mt-2 line-clamp-4">
-                {d.overview
-                  ? d.overview.slice(0, 70) + "..."
-                  : "No description available"}
-                <span className="text-blue-500 ml-1 underline"> more</span> 
-              
-              </p>
-            </div>
-          </Link>
+            <Link to={`/${d.media_type}/details/${d.id}`}>
+              <img
+                src={
+                  d.backdrop_path || d.poster_path
+                    ? `https://image.tmdb.org/t/p/original/${
+                        d.backdrop_path || d.poster_path
+                      }`
+                    : noimage
+                }
+                alt={d.name || d.title}
+                className="w-full h-3/4 object-cover"
+              />
+              <div className="p-4">
+                <h1 className="text-lg font-bold text-white truncate">
+                  {d.name || d.title || d.original_name || d.original_title}
+                </h1>
+                <p className="text-sm text-zinc-100 mt-2 line-clamp-3">
+                  {d.overview
+                    ? d.overview.slice(0, 70) + "..."
+                    : "No description available"}
+                  <span className="text-blue-500 ml-1 underline"> more</span>
+                </p>
+              </div>
+            </Link>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
-  ) : (
-    <h1 className="text-white bg-zinc-800 font-black flex items-center justify-center text-5xl h-screen w-screen">
-      <Loading />
-    </h1>
   );
 };
 
