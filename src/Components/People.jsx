69a38aec29e1580f55a6a6c31ea81c5cc694d4f1
Shikhar <shikharvrm123@gmail.com>
2025-03-25 01:49:13 +0530

    Updated People

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import Topbar from "../Partials/Topbar";
-import Dropdown from "../Partials/Dropdown";
-import { useState } from "react";
 import axios from "../Utiliss/axios";
 import noimage from "/noimage.png";
-import { Link } from "react-router-dom";
 import Loading from "../Partials/Loading";
-
 
 const People = () => {
   document.title = "People";
-
 
   const navigate = useNavigate();
   const [category, setCategory] = useState("popular");
@@ -30,62 +25,51 @@ const People = () => {
     getPeople(category);
   }, [category]);
 
-  const handleSelectChange = (value) => {
-    setCategory(value);
-  };
-
-
-  
-
-
-  return people.length>0 ? (
-    <div className="w-screen h-screen px-8 overflow-x-hidden text-white overflow-auto">
-      <div className="w-full h-[10vh] flex items-center justify-between">
-        <div className="flex items-center space-x-3">
+  return people.length > 0 ? (
+    <>
+      <Topbar />
+      <div className="w-full pb-[5vh] min-h-[screen] px-4 sm:px-8 lg:px-16 overflow-x-hidden text-white pt-[10vh]">
+        <div className="flex gap-2 items-center mb-10">
           <i
             onClick={() => navigate(-1)}
-            className="text-zinc-400 text-2xl relative z-40 hover:text-[#6556CD] ri-arrow-left-line mt-1 cursor-pointer"
-          ></i>
-          
-          <h1 className="font-bold text-2xl text-zinc-400">People <span className="text-[#6556CD]">({category})</span></h1>
+            className="text-zinc-400 text-2xl hover:text-[#6556CD] ri-arrow-left-line cursor-pointer"
+          />
+          <h1 className="font-bold text-xl md:text-2xl lg:text-3xl text-zinc-400">
+            Popular Peoples 
+          </h1>
         </div>
 
-        <div className="flex-1 flex backdrop-blur-0 bg-transparent ml-[-10%] z-10 p-7 justify-center ">
-          <Topbar />
-        </div>
-
-        <div className="flex space-x-4">
-          
+        <div className="grid gap-6 sm:gap-12 md:gap-12 lg:gap-14 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {people.map((c, i) => (
+            <Link
+              to={`/person/details/${c.id}`}
+              key={i}
+              className="group duration-500 transition-transform transform hover:translate-y-[-10px]"
+            >
+              <img
+                className="w-full h-[40vh] sm:h-[45vh] md:h-[50vh] object-cover rounded-2xl shadow-lg"
+                src={
+                  c.backdrop_path || c.profile_path || c.poster_path
+                    ? `https://image.tmdb.org/t/p/original/${
+                        c.backdrop_path || c.poster_path || c.profile_path
+                      }`
+                    : noimage
+                }
+                alt={c.name || "Person"}
+              />
+              <h1 className="text-lg sm:text-xl md:text-xl mt-3 font-bold group-hover:text-[#6556CD] transition">
+                {c.name || c.title || c.original_name || c.original_title}
+              </h1>
+            </Link>
+          ))}
         </div>
       </div>
-
-      <div className="flex flex-wrap justify-center w-full gap-x-7 mt-10">
-        {people.map((c, i) => (
-          <Link to={`/person/details/${c.id}`} key={i} className="w-[35vh]  ">
-            {" "}
-            <img
-              className="h-[45vh] object-cover hover:scale-105 duration-150 mb-3 rounded-2xl"
-              src={
-                c.backdrop_path || c.profile_path || c.poster_path
-                  ? `https://image.tmdb.org/t/p/original/${
-                      c.backdrop_path || c.poster_path || c.profile_path
-                    }`
-                  : noimage
-              }
-              alt=""
-            />
-            
-            <h1 className="text-xl mb-7 font-black hover:text-[#6556CD]">
-              {c.name || c.title || c.original_name || c.original_title}
-            </h1>
-          </Link>
-        ))}
-      </div>
+    </>
+  ) : (
+    <div className="flex items-center justify-center w-screen h-screen bg-black">
+      <Loading />
     </div>
-  ):( <h1 className="text-white bg-black font-black flex items-center justify-center text-5xl h-screen w-screen">
-    <Loading />
-  </h1>)
+  );
 };
 
 export default People;
- 
