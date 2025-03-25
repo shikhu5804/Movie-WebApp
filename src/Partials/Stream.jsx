3d829b6eee33debef75505b3cb6f18f248632a9a
Shikhar <shikharvrm123@gmail.com>
2025-03-25 14:52:44 +0530

    Responsive

--- src/Partials/Stream.jsx
+++ src/Partials/Stream.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Loading from "./Loading";
 
 const Stream = () => {
@@ -17,23 +17,26 @@ const Stream = () => {
 
   return (
     <div className="text-white overflow-hidden w-full h-full bg-[rgba(0,0,0,.9)] fixed z-20 top-0 left-0 flex justify-center items-center">
+      
+      {/* Close Button */}
       <i
         onClick={() => navigate(`/${category}/details/${id}`)}
-        className="text-zinc-300 absolute top-10 right-20 text-3xl hover:text-[#6556CD] ri-close-line cursor-pointer transition duration-300"
+        className="text-zinc-300 absolute top-6 right-6 sm:top-10 sm:right-20 text-3xl hover:text-[#6556CD] ri-close-line cursor-pointer transition duration-300 z-30"
       ></i>
 
       {loading && <Loading />}
 
-      <iframe
-        src={`https://vidsrc.me/embed/${category}/${imdb}`}
-        referrerpolicy="origin"
-        width="1200"
-        height="600"
-        frameBorder="0"
-        allowFullScreen
-        className="rounded-xl"
-        onLoad={handleIframeLoad}
-      ></iframe>
+      {/* Responsive Iframe */}
+      <div className="w-[95%] sm:w-[90%] md:w-[80%] lg:w-[70%] h-[70vh] sm:h-[75vh] md:h-[80vh] lg:h-[85vh] flex justify-center items-center">
+        <iframe
+          src={`https://vidsrc.me/embed/${category}/${imdb}`}
+          referrerPolicy="origin"
+          frameBorder="0"
+          allowFullScreen
+          className="w-full h-full rounded-xl shadow-lg"
+          onLoad={handleIframeLoad}
+        ></iframe>
+      </div>
     </div>
   );
 };
